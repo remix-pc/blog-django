13c9a78bb64bce51643e3e395c878a42ae445bde
remix-pc <guimoreiramanow.contato@gmail.com>
2023-04-13 14:53:05 -0300

    Filtrando as categorias

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
-
+from django.db.models import Q, Count, Case, When
 
 # Create your views here.
 
@@ -12,13 +12,59 @@ class PostIndex(ListView):
     template_name = 'posts/index.html'
     paginate_by = 2
     context_object_name = 'posts'
+    ordering = ['-id']
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs.filter(publishPost=True)
+        qs = qs.annotate(
+            commentNumber = Count(
+                Case(
+                    When(comment__publishComment=True, then=1)
+                )
+            )
+        )
+
+        return qs
+
+
 
 class PostSearch(PostIndex):
-    pass
+    template_name = 'posts/post_search.html'
 
+    def get_queryset(self):
+        qs =  super().get_queryset()
+
+        term = self.request.GET.get('termo')
+
+        if not term:
+            return qs
+
+        qs = qs.filter(
+            Q(titlePost__icontains=term) |
+            Q(author__first_name__iexact=term) |
+            Q(content__icontains=term) |
+            Q(excerpt__icontains=term) |
+            Q(category__name_cat__iexact=term)
+        )
+
+
+        return qs
 
 class PostCategory(PostIndex):
-    pass
+    template_name = 'posts/post_category.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        categorie = self.kwargs.get('category', None)
+
+        if not categorie:
+            return qs
+
+        qs = qs.filter(category__name_cat__iexact=categorie)
+
+        return qs
 
 class PostDetails(UpdateView):
     pass
