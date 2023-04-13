bf046ed16acecf46a7ebc2c05984f9e8f762fdc9
remix-pc <guimoreiramanow.contato@gmail.com>
2023-04-13 09:27:49 -0300

    Remoção do campo status e adicionado comentários

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Comment
 
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nameComment', 'emailComment', 'postComment', 'dateComment', 'publishComment')
+    list_editable = ('publishComment',)
+    list_display_links = ('id', 'nameComment', 'emailComment', )
+
+
+admin.site.register(Comment, CommentAdmin)
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from posts.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Comment(models.Model):
+    nameComment = models.CharField(max_length=150)
+    emailComment = models.EmailField()
+    comment = models.TextField()
+    postComment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    userComment = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    dateComment = models.DateTimeField(default=timezone.now)
+    publishComment = models.BooleanField(default=False)
